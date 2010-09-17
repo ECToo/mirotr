006a5a57663f280f2b2ed1925631ce506bc96016
admin@progandy.co.cc <admin@progandy.co.cc@eced67a3-f377-a0ae-92ae-d6de1850b05a>
2010-09-17 15:54:05 +0000

    - version bump, last (unsupported) ANSI-build

--- MirOTR/version.h
+++ MirOTR/version.h
@@ -4,11 +4,11 @@
 /* VERSION DEFINITIONS */
 #define VER_MAJOR				0
 #define VER_MINOR				10
-#define VER_RELEASE				2
-#define VER_BUILD				3
+#define VER_RELEASE				3
+#define VER_BUILD				1
 
 #define __STRINGIZE(x)			#x
-#define VER_STRING				"0.10.2.3"
+#define VER_STRING				"0.10.3.1"
 
 #ifdef _UNICODE
 	#define SHORT_NAME_STRING 		"Miranda OTR"
